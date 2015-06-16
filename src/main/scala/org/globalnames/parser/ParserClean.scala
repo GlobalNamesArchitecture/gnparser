5f9b38d047186f28d64719afb92bba21cadb3467
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-06-16 18:29:43 -0400

    name author year

--- src/main/scala/org/globalnames/parser/ParserClean.scala
+++ src/main/scala/org/globalnames/parser/ParserClean.scala
@@ -1,32 +1,60 @@
 package org.globalnames.parser
 
 import org.parboiled2._
-import scala.annotation.switch
-import scala.util.{ Try, Success, Failure }
 
 object ParseUtil {
+
   trait Line
+
   trait Word
 
 
   case class Comment() extends Line
+
   case class Blank() extends Line
+
   case class Name(verbatim: String) extends Line
+
   case class LatinWord(word: String) extends Word
+
   case class CapLatinWord(word: String) extends Word
+
 }
 
 class ParserClean(val input: ParserInput) extends Parser {
-  import CharPredicate.{Printable}
+
+  import CharPredicate.{Digit, Printable}
   import ParseUtil._
 
-  def line: Rule1[Line] = rule { noName | name }
+  def line: Rule1[Line] = rule {
+    noName | sciName
+  }
 
-  private def noName: Rule1[Line] = rule { blank | comment }
+  private def noName: Rule1[Line] = rule {
+    blank | comment
+  }
 
-  private def name: Rule1[Line] = rule {
-    softSpace ~ ( binomial | uninomial ) ~
+  private def sciName: Rule1[Line] = rule {
+    softSpace ~ (nameAuthor | name) ~
       softSpace ~ EOI ~> (x => Name(x))
+  }
+
+  private def authorship: Rule1[String] = rule {
+    authorYear | authorWord
+  }
+
+  private def authorYear: Rule1[String] = rule {
+    authorWord ~ softSpace ~ year ~> ((a: String, y: String) =>
+      s"$a $y".toString)
+  }
+
+  private def nameAuthor: Rule1[String] = rule {
+    name ~ space ~ authorship ~> ((w1: String, w2: String) =>
+      s"$w1 $w2".toString)
+  }
+
+  private def name: Rule1[String] = rule {
+    binomial | uninomial
   }
 
   private def blank: Rule1[Line] = rule {
@@ -40,17 +68,17 @@ class ParserClean(val input: ParserInput) extends Parser {
 
   private def binomial: Rule1[String] = rule {
     capWord ~ space ~ word ~> ((w1: String, w2: String) =>
-    s"$w1 $w2".toString)
+      s"$w1 $w2".toString)
   }
 
   private def uninomial: Rule1[String] = rule {
     capWord
   }
-//
-//  private def authorWord: Rule1[String] = rule {
-//    capture(CharPredicate.UpperAlpha ~
-//      CharPredicate.LowerAlpha ~ '.'.?)
-//  }
+
+  private def authorWord: Rule1[String] = rule {
+    capture(CharPredicate.UpperAlpha ~
+      zeroOrMore(CharPredicate.LowerAlpha) ~ '.'.?)
+  }
 
   private def capWord: Rule1[String] = rule {
     capture(upperChar ~ oneOrMore(lowerChar))
@@ -68,9 +96,20 @@ class ParserClean(val input: ParserInput) extends Parser {
     CharPredicate("abcdefghijklmnopqrstuvwxyzëæœ")
   }
 
-  private def softSpace = rule { zeroOrMore(spaceChars) }
+  private def year: Rule1[String] = rule {
+    capture(CharPredicate("12") ~
+      CharPredicate("0789") ~ Digit ~ Digit)
+  }
 
-  private def space = rule { oneOrMore(spaceChars) }
+  private def softSpace = rule {
+    zeroOrMore(spaceChars)
+  }
 
-  private def spaceChars = rule { CharPredicate(" \t\r\n\f") }
+  private def space = rule {
+    oneOrMore(spaceChars)
+  }
+
+  private def spaceChars = rule {
+    CharPredicate(" \t\r\n\f")
+  }
 }
--- src/main/scala/org/globalnames/parser/Parserver.scala
+++ src/main/scala/org/globalnames/parser/Parserver.scala
@@ -1,15 +1,32 @@
 package org.globalnames.parser
 
+import scala.util.{Try, Success, Failure}
+import org.globalnames.parser.ParseUtil._
+import org.parboiled2._
+
 object Parserver {
   def main(args: Array[String]) {
     val doc = """
     # Comment
     Betula
     Betula alba
+    Betula alba       Linnaeus
+    Betula alba Linn.
+    Betula alba L. 1758
     """
 
-    val parsed = doc.lines map { line => new ParserClean(line).line.run() }
-
-    parsed foreach println
+    val parsed = doc.lines foreach { line => pparse(line) }
   }
+
+  private def pparse(name: String) {
+    val pc = new ParserClean(name)
+    val parsed = pc.line.run()
+    parsed match {
+      case Success(x: Name) => println(x.verbatim)
+      case Success(_) =>
+      case Failure(e: ParseError) => println(pc.formatError(e))
+      case Failure(e) => println(e)
+    }
+  }
+
 }
