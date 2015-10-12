8548c157fd666140055063fe153e55659eca0067
Alexander Myltsev <alexander.myltsev@phystech.edu>
2015-10-12 22:50:25 +0300

    = Remove `showWarnings` parameter

--- examples/java/src/main/java/Parser.java
+++ examples/java/src/main/java/Parser.java
@@ -3,7 +3,7 @@ import org.globalnames.parser.ScientificNameParser;
 public class Parser {
     public static void main(String[] args) {
         ScientificNameParser.Result result =
-            ScientificNameParser.instance().fromString("Homo sapiens L.", false);
+            ScientificNameParser.instance().fromString("Homo sapiens L.");
         String jsonStr = ScientificNameParser.instance().renderCompactJson(result);
         System.out.println(jsonStr);
     }
--- examples/jython/parser.py
+++ examples/jython/parser.py
@@ -1,5 +1,5 @@
 from org.globalnames.parser import ScientificNameParser
 
 snp = ScientificNameParser.instance()
-result = snp.fromString("Homo sapiens L.", False)
+result = snp.fromString("Homo sapiens L.")
 print snp.renderCompactJson(result)
