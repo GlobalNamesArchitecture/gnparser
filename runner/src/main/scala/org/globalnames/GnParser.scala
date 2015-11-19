5213e37bed4a2942bcedd469660ee80cf4e35d10
Alexander Myltsev <alexander.myltsev@phystech.edu>
2015-11-19 15:11:39 +0300

    = Implement raw threads parallelization

--- runner/src/main/scala/org/globalnames/GnParser.scala
+++ runner/src/main/scala/org/globalnames/GnParser.scala
@@ -1,10 +1,12 @@
 package org.globalnames
 
 import java.io.{BufferedWriter, FileWriter}
+import java.text.NumberFormat
+import java.util.Locale
+import java.util.concurrent.{CountDownLatch, Executors}
 
 import org.globalnames.parser.ScientificNameParser.{instance ⇒ scientificNameParser}
 
-import scala.collection.parallel.ForkJoinTaskSupport
 import scala.concurrent.forkjoin.ForkJoinPool
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
@@ -49,20 +51,49 @@ object GnParser {
         case Success(f) =>
           val parallelism = Option(sys.props("parallelism")).map { _.toInt }
             .getOrElse(ForkJoinPool.getCommonPoolParallelism)
-          println(s"running with parallelism: $parallelism")
+          val batchSize = Option(sys.props("batch")).map { _.toInt }
+            .getOrElse(100)
+          println(s"running with: parallelism=$parallelism batch=$batchSize")
+          val namesInput = f.getLines().toArray
+          val pool = Executors.newFixedThreadPool(parallelism)
           val parsedNamesCount = new java.util.concurrent.atomic.AtomicInteger()
-          val namesInput = f.getLines().toVector.par
-          namesInput.tasksupport =
-            new ForkJoinTaskSupport(new ForkJoinPool(parallelism))
-          val namesParsed = namesInput.map { name ⇒
-            val currentParsedCount = parsedNamesCount.incrementAndGet()
-            if (currentParsedCount % 10000 == 0) {
-              println(s"Parsed $currentParsedCount of ${namesInput.size} lines")
-            }
-            scientificNameParser.fromString(name.trim).renderCompactJson
+          val workersActive = new CountDownLatch(parallelism)
+          val start = System.nanoTime()
+          for (_ <- 1 to parallelism) {
+            pool.submit(new Runnable {
+              override def run(): Unit = {
+                var parsing = true
+                while (parsing) {
+                  val currentParsedNamesCount = parsedNamesCount.getAndIncrement()
+                  parsing = currentParsedNamesCount < namesInput.length / batchSize
+                  if (parsing) {
+                    if (currentParsedNamesCount % 100 == 0) {
+                      println(
+                        s"""Parsed ${currentParsedNamesCount * batchSize} """ +
+                        s"""of ${namesInput.length} lines""")
+                    }
+                    val beginIdx = currentParsedNamesCount * batchSize
+                    val endIdx = math.min((currentParsedNamesCount + 1) * batchSize,
+                      namesInput.length)
+                    for (i ← beginIdx until endIdx) {
+                      val name = namesInput(i).trim
+                      val result = scientificNameParser.fromString(name)
+                      namesInput(i) = result.renderCompactJson
+                    }
+                  }
+                }
+                workersActive.countDown()
+              }
+            })
           }
+          workersActive.await()
+          val end = System.nanoTime()
+          println("Processed for " +
+            NumberFormat.getNumberInstance(Locale.US).format((end - start)/1000000) +
+            "ms")
+          pool.shutdown()
           val writer = new BufferedWriter(new FileWriter(outputFilePath))
-          namesParsed.seq.foreach { name ⇒
+          namesInput.seq.foreach { name ⇒
             writer.write(name + System.lineSeparator)
           }
           writer.close()
