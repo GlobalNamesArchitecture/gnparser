eed32184f253894e0acb62f7dcb46c5b993a1caa
Alexander Myltsev <alexander@myltsev.com>
2016-05-18 11:29:27 +0300

    + Add Apache Spark example in Scala

---
+++ examples/spark/src/main/scala/org/globalnames/parser/examples/ParserSpark.scala
@@ -0,0 +1,20 @@
+package org.globalnames.parser.examples
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.globalnames.parser.ScientificNameParser.{instance => snp}
+
+object ParserSpark {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("abc")
+    val sc   = new SparkContext(conf)
+
+    val names = List("Homo Sapiens Linnaeus 1758",
+                     "Salinator solida Martens 1878",
+                     "Kanisamys indicus Wood 1937")
+    val canonicals = sc.parallelize(names).map { name =>
+      val canonical = snp.fromString(name).canonized()
+      canonical.getOrElse("")
+    }
+    println(canonicals.collect().mkString("\n"))
+  }
+}
