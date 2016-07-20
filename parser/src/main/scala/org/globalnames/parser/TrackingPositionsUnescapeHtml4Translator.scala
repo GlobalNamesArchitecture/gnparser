4af098eebeb1d56af63ea57d49921732a461ec89
Alexander Myltsev <alexander@myltsev.com>
2016-07-20 16:51:47 +0300

    = Extract translators to static object

--- parser/src/main/scala/org/globalnames/parser/TrackingPositionsUnescapeHtml4Translator.scala
+++ parser/src/main/scala/org/globalnames/parser/TrackingPositionsUnescapeHtml4Translator.scala
@@ -7,28 +7,31 @@ import org.apache.commons.lang3.text.translate._
 import scala.collection.mutable.ArrayBuffer
 
 class TrackingPositionsUnescapeHtml4Translator extends AggregateTranslator {
-  private final val translators = {
-    def lookupTranslator(strss: Array[Array[String]]) =
-      new LookupTranslator(strss.asInstanceOf[Array[Array[CharSequence]]]: _*)
-    List(
-      lookupTranslator(EntityArrays.BASIC_UNESCAPE),
-      lookupTranslator(EntityArrays.ISO8859_1_UNESCAPE),
-      lookupTranslator(EntityArrays.HTML40_EXTENDED_UNESCAPE()),
-      new NumericEntityUnescaper)
-  }
+  import TrackingPositionsUnescapeHtml4Translator.translators
 
   private final val positions = ArrayBuffer.fill(1)(0)
-  var identity: Boolean = true
+  final var identity: Boolean = true
 
   override def translate(input: CharSequence, index: Int, out: Writer): Int = {
     val consumed = translators.foldLeft(0) { (cnsmed, cst) =>
       if (cnsmed == 0) cst.translate(input, index, out)
       else cnsmed
     }
-    positions += positions.last + (if (consumed == 0) 1
-                                   else { identity = false; consumed })
+    identity &&= consumed == 0
+    positions += positions.last + (if (consumed == 0) 1 else consumed)
     consumed
   }
 
   def at(pos: Int): Int = if (identity) pos else positions(pos)
+}
+
+object TrackingPositionsUnescapeHtml4Translator {
+  private[TrackingPositionsUnescapeHtml4Translator] final val translators = {
+    val translator1 = new LookupTranslator(
+      (EntityArrays.BASIC_UNESCAPE ++
+        EntityArrays.ISO8859_1_UNESCAPE ++
+        EntityArrays.HTML40_EXTENDED_UNESCAPE).asInstanceOf[Array[Array[CharSequence]]]: _*)
+    val translator2 = new NumericEntityUnescaper
+    List(translator1, translator2)
+  }
 }
