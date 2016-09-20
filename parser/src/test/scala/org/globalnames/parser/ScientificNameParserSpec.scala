ae8e45cad13da6ddd5eb5c95154a42e239942090
Alexander Myltsev <alexander@myltsev.com>
2016-09-20 14:15:28 +0300

    = Extract `ExpectedName` in Spec

--- parser/src/test/scala/org/globalnames/parser/ScientificNameParserSpec.scala
+++ parser/src/test/scala/org/globalnames/parser/ScientificNameParserSpec.scala
@@ -7,6 +7,7 @@ import org.specs2.mutable.Specification
 import scala.io.Source
 
 class ScientificNameParserSpec extends Specification {
+  case class ExpectedName(verbatim: String, json: String, simple: String)
 
   "ScientificNameParser specification".p
 
@@ -14,28 +15,25 @@ class ScientificNameParserSpec extends Specification {
     val version = "test_version"
   }
 
-  def expectedNames(filePath: String): Vector[(String, String, String)] = {
+  def expectedNames(filePath: String): Vector[ExpectedName] = {
     Source.fromURL(getClass.getResource(filePath), "UTF-8")
           .getLines
           .takeWhile { _.trim != "__END__" }
-          .withFilter { line =>
-           !(line.isEmpty || ("#\r\n\f\t" contains line.charAt(0)))
-          }
+          .withFilter { line => !(line.isEmpty || ("#\r\n\f\t" contains line.charAt(0))) }
           .sliding(3, 3)
-          .map { ls => (ls(0), ls(1), ls(2)) }
+          .map { ls => ExpectedName(ls(0), ls(1), ls(2)) }
           .toVector
   }
 
-  expectedNames("/test_data.txt").foreach {
-    case (inputStr, expectedJsonStr, expectedDelimitedStr) =>
-      val json = parse(expectedJsonStr)
-      val jsonParsed = scientificNameParser.fromString(inputStr).json()
+  expectedNames("/test_data.txt").foreach { expectedName =>
+      val json = parse(expectedName.json)
+      val jsonParsed = scientificNameParser.fromString(expectedName.verbatim).json()
                          .removeField { case (_, v) => v == JNothing }
 
       val jsonDiff = {
         val Diff(changed, added, deleted) = jsonParsed.diff(json)
         s"""line:
-           |$inputStr
+           |${expectedName.verbatim}
            |parsed:
            |${pretty(jsonParsed)}
            |test_data:
@@ -48,17 +46,17 @@ class ScientificNameParserSpec extends Specification {
            |${pretty(deleted)}""".stripMargin
       }
 
-      s"parse correctly: '$inputStr'" in {
+      s"parse correctly: '${expectedName.verbatim}'" in {
         s"original json must match expected one:\n $jsonDiff" ==> {
           json === jsonParsed
         }
       }
 
       val Array(uuid, verbatim, canonical, canonicalExtended,
-                authorship, year, quality) = expectedDelimitedStr.split('|')
+                authorship, year, quality) = expectedName.simple.split('|')
 
-      s"parse correctly delimited string: '$inputStr'" in {
-        val pr = scientificNameParser.fromString(inputStr)
+      s"parse correctly delimited string: '${expectedName.verbatim}'" in {
+        val pr = scientificNameParser.fromString(expectedName.verbatim)
 
         uuid              === pr.input.id.toString
         verbatim          === pr.input.verbatim
