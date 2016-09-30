3179ca46d755064a41976cc7fe2c7ce466a660c6
Alexander Myltsev <alexander@myltsev.com>
2016-09-30 17:45:55 +0300

    = Fix test message

--- parser/src/test/scala/org/globalnames/UuidGeneratorSpec.scala
+++ parser/src/test/scala/org/globalnames/UuidGeneratorSpec.scala
@@ -7,7 +7,7 @@ import org.specs2.mutable.Specification
 class UuidGeneratorSpec extends Specification {
   val gen = UuidGenerator()
 
-  "Generates:" >> {
+  "Generates UUID correctly" >> {
     gen.generate("Pseudocercospora Speg.") ===
       UUID.fromString("ccc7780b-c68b-53c6-9166-6b2d4902923e")
   }
