657f43a1e864bd00efba950d965cb8aa2289091b
Alexander Myltsev <alexander@myltsev.com>
2017-06-25 11:25:00 +0300

    Add Jython example testing

--- examples/jython/parser.py
+++ examples/jython/parser.py
@@ -1,5 +1,11 @@
-from org.globalnames.parser import ScientificNameParser
+import os
+import sys
+
+gnparser_jar_path = os.environ["GNPARSER_JAR_PATH"]
+sys.path.append(gnparser_jar_path)
+
 import json
+from org.globalnames.parser import ScientificNameParser
 
 snp = ScientificNameParser.instance()
 result = snp.fromString("Homo sapiens L.").renderCompactJson()
@@ -8,3 +14,5 @@ json_result = json.loads(result)
 assert json_result["parsed"]
 assert json_result["positions"] == \
         [["genus", 0, 4], ["specific_epithet", 5, 12], ["author_word", 13, 15]]
+
+print "Name is parsed:\n", json_result
