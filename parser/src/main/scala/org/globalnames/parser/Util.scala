19e19bf21459afbb85993c1de43dc117a8a43790
Alexander Myltsev <alexander@myltsev.com>
2017-09-20 17:54:00 +0300

    Fix #386: Update Util so it doesn't break tests

--- parser/src/main/scala/org/globalnames/parser/Util.scala
+++ parser/src/main/scala/org/globalnames/parser/Util.scala
@@ -23,55 +23,64 @@ object Util {
 
   def normalize(input: String): String = {
     val output = new StringBuilder()
-    for (chr <- input) {
-      if (charMapping.contains(chr)) {
-        output.append(charMapping(chr))
+    var inputIdx = 0
+    while (inputIdx < input.length) {
+      val chr = input.charAt(inputIdx)
+      if (inputIdx == 0 && chr.isDigit) {
+        while (input.charAt(inputIdx).isDigit) {
+          inputIdx += 1
+        }
+        output.append(numberToString(input.substring(0, inputIdx)))
+        if (Set('.', '-').contains(input.charAt(inputIdx))) {
+          inputIdx += 1
+        }
       } else {
-        output.append(chr)
+        if (charMapping.contains(chr)) {
+          output.append(charMapping(chr))
+        } else {
+          output.append(chr)
+        }
+        inputIdx += 1
       }
     }
     val res = output.toString.replaceFirst("""\?$""", "")
-    if (res(0).isDigit) numsToString(res) else res
+    res
   }
 
-  private def numsToString(input: String): String = {
-    val (numeral, suffix) = input.splitAt(input.indexOf('-') + 1)
-    val prefix = numeral match {
-        case "1-"  => "uni"
-        case "2-"  => "bi"
-        case "3-"  => "tri"
-        case "4-"  => "quadri"
-        case "5-"  => "quinque"
-        case "6-"  => "sex"
-        case "7-"  => "septem"
-        case "8-"  => "octo"
-        case "9-"  => "novem"
-        case "10-" => "decem"
-        case "11-" => "undecim"
-        case "12-" => "duodecim"
-        case "13-" => "tredecim"
-        case "14-" => "quatuordecim"
-        case "15-" => "quindecim"
-        case "16-" => "sedecim"
-        case "17-" => "septendecim"
-        case "18-" => "octodecim"
-        case "19-" => "novemdecim"
-        case "20-" => "viginti"
-        case "21-" => "vigintiuno"
-        case "22-" => "vigintiduo"
-        case "23-" => "vigintitre"
-        case "24-" => "vigintiquatuor"
-        case "25-" => "vigintiquinque"
-        case "26-" => "vigintisex"
-        case "27-" => "vigintiseptem"
-        case "28-" => "vigintiocto"
-        case "30-" => "triginta"
-        case "31-" => "trigintauno"
-        case "32-" => "trigintaduo"
-        case "38-" => "trigintaocto"
-        case "40-" => "quadraginta"
-        case _     => numeral
-      }
-    prefix + suffix
+  private def numberToString(numeral: String): String = numeral match {
+    case "1"  => "uni"
+    case "2"  => "bi"
+    case "3"  => "tri"
+    case "4"  => "quadri"
+    case "5"  => "quinque"
+    case "6"  => "sex"
+    case "7"  => "septem"
+    case "8"  => "octo"
+    case "9"  => "novem"
+    case "10" => "decem"
+    case "11" => "undecim"
+    case "12" => "duodecim"
+    case "13" => "tredecim"
+    case "14" => "quatuordecim"
+    case "15" => "quindecim"
+    case "16" => "sedecim"
+    case "17" => "septendecim"
+    case "18" => "octodecim"
+    case "19" => "novemdecim"
+    case "20" => "viginti"
+    case "21" => "vigintiuno"
+    case "22" => "vigintiduo"
+    case "23" => "vigintitre"
+    case "24" => "vigintiquatuor"
+    case "25" => "vigintiquinque"
+    case "26" => "vigintisex"
+    case "27" => "vigintiseptem"
+    case "28" => "vigintiocto"
+    case "30" => "triginta"
+    case "31" => "trigintauno"
+    case "32" => "trigintaduo"
+    case "38" => "trigintaocto"
+    case "40" => "quadraginta"
+    case _    => numeral
   }
 }
