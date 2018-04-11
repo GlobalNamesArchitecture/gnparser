a69296f08c12a71fdb9493bd21a39c3730c5bb9b
Alexander Myltsev <alexander@myltsev.com>
2018-04-11 23:27:06 +0300

    Fix `AuthorsTeam.pos`

--- parser/src/main/scala/org/globalnames/parser/AST.scala
+++ parser/src/main/scala/org/globalnames/parser/AST.scala
@@ -167,15 +167,14 @@ case class AuthorsTeam(authors: Seq[Author],
   }
 
   val pos: CapturePosition = {
-    val start = authors.headOption match {
-      case Some(a) => a.pos.start
-      case None => years.headOption.map { _.pos.start }.getOrElse(-1)
+    val poss = authors.map { _.pos } ++ years.map { _.pos }
+    if (poss.isEmpty) {
+      CapturePosition.empty
+    } else {
+      val start = poss.minBy { _.start }.start
+      val end = poss.maxBy { _.end }.end
+      CapturePosition(start, end)
     }
-    val end = years.lastOption match {
-      case Some(y) => y.pos.end
-      case None => authors.lastOption.map { _.pos.end }.getOrElse(-1)
-    }
-    CapturePosition(start, end)
   }
 }
 
