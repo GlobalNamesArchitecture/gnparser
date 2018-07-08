08f4550fdfa3c2f6b9a3c93ef8d35e6d92e40af5
Alexander Myltsev <alexander@myltsev.com>
2018-07-08 15:21:00 +0300

    Fix #463: properly handle the positioning

--- parser/src/main/scala/org/globalnames/parser/TrackingPositionsUnescapeHtml4Translator.scala
+++ parser/src/main/scala/org/globalnames/parser/TrackingPositionsUnescapeHtml4Translator.scala
@@ -1,6 +1,6 @@
 package org.globalnames.parser
 
-import java.io.Writer
+import java.io.{StringWriter, Writer}
 
 import org.apache.commons.text.translate._
 
@@ -10,13 +10,37 @@ import scala.collection.JavaConverters._
 class TrackingPositionsUnescapeHtml4Translator extends AggregateTranslator {
   import TrackingPositionsUnescapeHtml4Translator.translators
 
-  private final val positions = ArrayBuffer.fill(1)(0)
+  final var positions: ArrayBuffer[Int] = ArrayBuffer[Int]()
   final var identity: Boolean = true
+  final var indexCurrent: Int = _
+
+  def translate(input: String): String = {
+    // NOTE: `override def translate(input: CharSequence, index: Int, out: Writer): Int`
+    // has no friendly way to track the positioning. Consider tracking positions for
+    // `<i>Homo</i> sapiens Linn. &amp; Markov`: `<i>` maps to empty string, and `&amp;` to `&`.
+    // And the translator needs to track if the `Char` was actually added to the output to track it
+    // Hence, we're redefining the `translate` method to keep track on the `out: Writer`
+
+    val stringWriter = new StringWriter(input.length * 2) {
+      override def write(ch: Int): Unit = {
+        positions += indexCurrent
+        super.write(ch)
+      }
+
+      override def write(str: String): Unit = {
+        (0 until str.length).foreach { _ => positions += indexCurrent }
+        super.write(str)
+      }
+    }
+
+    this.translate(input, stringWriter)
+    stringWriter.toString
+  }
 
   override def translate(input: CharSequence, index: Int, out: Writer): Int = {
+    indexCurrent = index
     val consumed = translators.translate(input, index, out)
     identity &&= consumed == 0
-    positions += positions.last + (if (consumed == 0) 1 else consumed)
     consumed
   }
 
--- parser/src/test/scala/org/globalnames/parser/TrackingPositionsUnescapeHtml4TranslatorSpec.scala
+++ parser/src/test/scala/org/globalnames/parser/TrackingPositionsUnescapeHtml4TranslatorSpec.scala
@@ -6,40 +6,45 @@ class TrackingPositionsUnescapeHtml4TranslatorSpec extends Specification {
   def positions(input: String): List[Int] = {
     val t = new TrackingPositionsUnescapeHtml4Translator()
     val translated = t.translate(input)
-    List.range(0, translated.length + 1).map(t.at)
+    val positions = for (idx <- 0 until translated.length) yield t.at(idx)
+    positions.toList
   }
 
   "Unescape translator should correctly handle" >> {
     "t" in {
-      positions("t") === List(0, 1)
+      positions("t") === List(0)
     }
 
     "Æ" in {
-      positions("&AElig;") === List(0, 7)
+      positions("&AElig;") === List(0)
     }
 
     "Tt" in {
-      positions("Tt") === List(0, 1, 2)
+      positions("Tt") === List(0, 1)
     }
 
     "Æt" in {
-      positions("&AElig;t") === List(0, 7, 8)
+      positions("&AElig;t") === List(0, 7)
     }
 
     "Tð" in {
-      positions("T&eth;") === List(0, 1, 6)
+      positions("T&eth;") === List(0, 1)
     }
 
     "Æð" in {
-      positions("&AElig;&eth;") === List(0, 7, 12)
+      positions("&AElig;&eth;") === List(0, 7)
     }
 
     "Ætð" in {
-      positions("&AElig;t&eth;") === List(0, 7, 8, 13)
+      positions("&AElig;t&eth;") === List(0, 7, 8)
     }
 
     "<i>p</i>" in {
-      positions("<i>p</i>") === List(0, 3)
+      positions("<i>p</i>") === List(3)
+    }
+
+    "&amp;p&nbsp;a</i>l<i>" in {
+      positions("&amp;p&nbsp;a</i>l<i>") === List(0, 5, 6, 12, 17)
     }
   }
 }
